37206f84de27ed7be071a6fe9d056bc64d706f08
sebe <sebe2k04@gmail.com>
2024-09-16 07:09:01 +0530

    cookie fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -34,7 +34,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "None",
+      sameSite: "Strict",
     });
 
     res.status(200).json({ message: "Login successful" });
