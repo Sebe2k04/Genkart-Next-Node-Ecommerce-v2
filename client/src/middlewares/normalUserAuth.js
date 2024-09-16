d5a102e52421ccd18bc3398fb334ddd8611d4af7
sebe <sebe2k04@gmail.com>
2024-09-16 06:58:28 +0530

    console checks

--- client/src/middlewares/normalUserAuth.js
+++ client/src/middlewares/normalUserAuth.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export const normalUserAuth = async (req) => {
   const token = req.cookies.get("token")?.value;
-  console.log(token);
+  console.log(token,"token");
   if (!token) {
     // Redirect to login if no token is found
     return NextResponse.redirect(new URL("/login", req.url));
@@ -11,6 +11,7 @@ export const normalUserAuth = async (req) => {
 
   try {
     // Verify JWT token
+    console.log("token verifying")
     const { payload } = await jwtVerify(
       token,
       new TextEncoder().encode(process.env.NEXT_PUBLIC_JWT_USER_SECRET)
@@ -19,6 +20,7 @@ export const normalUserAuth = async (req) => {
     // Proceed to the requested page
     return null;
   } catch (error) {
+    console.log("token validation failed")
     // Token verification failed, redirect to login
     return NextResponse.redirect(new URL("/login", req.url));
   }
