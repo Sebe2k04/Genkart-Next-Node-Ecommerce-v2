288195b97cd5fa2e1900bfe12c44697309c851bd
sebe <sebe2k04@gmail.com>
2024-09-16 18:22:46 +0530

    cookie --fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,7 +32,7 @@ const login = async (req, res) => {
     );
 
     res.cookie("userToken", token, {
-      domain:process.env.DOMAIN_NAME,
+      // domain:process.env.DOMAIN_NAME,
       httpOnly: true,
       path: "/",
       secure: process.env.NODE_ENV === "production",
