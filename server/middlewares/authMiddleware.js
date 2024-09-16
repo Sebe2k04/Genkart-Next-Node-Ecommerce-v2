a52f8af6a1c2d09e11d723a1ac127d57b7b4f741
sebe <sebe2k04@gmail.com>
2024-09-16 17:27:46 +0530

    console --fix

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
   const token = req.cookies.token;
+  console.log("token",token)
   if (!token) {
     return res.status(401).json({ message: "Not authenticated" });
   }
